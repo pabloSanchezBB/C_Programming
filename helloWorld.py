d9ca4f19fce49d81388c864c92924fe52985d7eb
pabloSanchezBB <t.rowland711@gmail.com>
2021-05-23 17:32:40 -0500

    updated to just contain C stuff

--- helloWorld.py
+++
@@ -1,8 +0,0 @@
-//helloWorld python
-
-print("Hello World!")
-
-int x = 1
-
-if x==1:
-	print("Hello World!")
--- myPrograms/calculator.c
+++
@@ -1,9 +0,0 @@
-//My Own Program: A Simple Calculator
-
-#include <stdio.h>
-
-int main(void){
-
-	printf("Please Enter the First Number:\n");
-	printf("Please Enter the Second Number:\n");
-}
